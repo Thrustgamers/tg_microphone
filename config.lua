974e8a87dd6d18c1c14932653d6f2346368c6490
Thrustgamers <94409067+Thrustgamers@users.noreply.github.com>
2022-10-02 01:37:14 +0200

    explanation added

--- config.lua
+++ config.lua
@@ -1,6 +1,7 @@
 Config = {}
 
 Config.Zones = {
+    --[number] = {coords = vector3 coords, spawnProp = true/false, context = "small/medium/large"},
     [1] = {coords = vector3(683.37, 569.31, 130.46), spawnProp = true, context = "large"},
     [2] = {coords = vector3(440.72, -985.81, 34.97), spawnProp = true, context = "medium"},
     [3] = {coords = vector3(-75.28, -818.7, 326.17), spawnProp = true, context = "small"},
